888e87bceb467f6879956dac3b46bac1cbf12e30
Egor Kutselabskii <osutanfirst@gmail.com>
2021-11-04 17:08:24 +1000

    Change player control system to be able to use InputRecorder later

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,22 +10,63 @@ public class Player : MonoBehaviour
 	private float moveSpeed = 10f;
 	private Vector3 movementVector;
 
+	private readonly KeyCode[] controlKeys = { KeyCode.A, KeyCode.D, KeyCode.W, KeyCode.S, KeyCode.Mouse0 };	// TODO Use enum
+	private HashSet<KeyCode> pressedKeys;
+	private bool manualControl = true;
+
 	void Start()
-    {
-        
-    }
+	{
+		pressedKeys = new HashSet<KeyCode>();
+	}
 
 	void Update()
 	{
-		movementVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		if (manualControl) {
+			GetManualInputs();
+		}
 
-		var mousePosition = FindObjectOfType<Camera>().ScreenToWorldPoint(Input.mousePosition);
+		var mousePosition = FindObjectOfType<Camera>().ScreenToWorldPoint(Input.mousePosition); // TODO Handle later somehow
 		transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) - 90f);
 
-		if (Input.GetKeyDown(KeyCode.Mouse0)) {
-			var bullet = Instantiate(Bullet);
-			bullet.Initialize(transform.position, transform.up);
+		movementVector = Vector3.zero;
+		foreach (var key in pressedKeys) {
+			switch (key) {
+				case KeyCode.A:
+					movementVector.x = -1;
+					break;
+				case KeyCode.D:
+					movementVector.x = 1;
+					break;
+				case KeyCode.S:
+					movementVector.y = -1;
+					break;
+				case KeyCode.W:
+					movementVector.y = 1;
+					break;
+				case KeyCode.Mouse0:
+					var bullet = Instantiate(Bullet);
+					bullet.Initialize(transform.position, transform.up);
+					break;
+			}
 		}
+	}
+
+	private void GetManualInputs()
+	{
+		foreach (var key in controlKeys) {
+			if (Input.GetKeyDown(key)) {
+				pressedKeys.Add(key);
+			}
+
+			if (Input.GetKeyUp(key)) {
+				pressedKeys.Remove(key);
+			}
+		}
+	}
+
+	private void GetRecordedInputs()
+	{
+		// TODO Use InputRecorder
 	}
 
 	void FixedUpdate()
